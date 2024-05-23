dea78d600cd5faeb23f2a7e754e9d96ca7f701b5
bharatbudhori <70325600+bharatbudhori@users.noreply.github.com>
2024-05-23 13:06:55 +0530

    fix language chnage

--- client/src/components/CodeDisplay/CodeEditor.jsx
+++ client/src/components/CodeDisplay/CodeEditor.jsx
@@ -8,70 +8,78 @@ import { DEFAUTL_CODE } from "../../constants";
 // import SubmitReport from "./sub-components/SubmitReport";
 
 const CodeEditor = ({ socket, displayName, roomId, friendText }) => {
-  const { theme, language } = useContext(CodeEditorContext);
-  const { editorRef } = useContext(CodeEditorContext);
-  const { username } = useContext(GlobalContext);
+    const { theme, language } = useContext(CodeEditorContext);
+    const { editorRef } = useContext(CodeEditorContext);
+    const { username } = useContext(GlobalContext);
 
-  // const editorRef = useRef(null);
+    // const editorRef = useRef(null);
 
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
-  }
+    function handleEditorDidMount(editor, monaco) {
+        editorRef.current = editor;
+    }
 
-  //   useEffect(() => {
-  //     console.log(friendText);
-  // }, [friendText]);
+    useEffect(() => {
+        if (socket == null) return;
 
-  return (
-    <div className="bg-gray-900 text-white">
-      <CodeEditorTop
-        socket={socket}
-        roomId={roomId}
-        displayName={displayName}
-      />
-      <div className="bg-gray-900 h-[600edpx]">
-        {/* <SubmitReport
+        socket.emit("sendMessageToRoom", {
+            room: roomId,
+            message: DEFAUTL_CODE,
+            from: username,
+            to: username,
+        });
+    }, []);
+
+    return (
+        <div className="bg-gray-900 text-white">
+            <CodeEditorTop
+                socket={socket}
+                roomId={roomId}
+                displayName={displayName}
+            />
+            <div className="bg-gray-900 h-[600edpx]">
+                {/* <SubmitReport
           accepted={accepted}
           showAccepted={showAccepted}
           setShowAccepted={setShowAccepted}
         /> */}
-        <Editor
-          height="44vh"
-          defaultLanguage={language}
-          defaultValue={DEFAUTL_CODE}
-          options={{
-            wordWrap: "on",
-          }}
-          onMount={() => {
-            handleEditorDidMount();
-            if (socket == null) return;
+                <Editor
+                    height="44vh"
+                    defaultLanguage={language}
+                    defaultValue={DEFAUTL_CODE}
+                    options={{
+                        wordWrap: "on",
+                    }}
+                    // onMount={() => {
+                    //   handleEditorDidMount();
+                    //   if (socket == null) return;
 
-            socket.emit("sendMessageToRoom", {
-              room: roomId,
-              message: DEFAUTL_CODE,
-              from: username,
-              to: username,
-            });
-          }}
-          onChange={(value, event) => {
-            if (socket == null) return;
+                    //   socket.emit("sendMessageToRoom", {
+                    //     room: roomId,
+                    //     message: DEFAUTL_CODE,
+                    //     from: username,
+                    //     to: username,
+                    //   });
+                    // }}
+                    onMount={handleEditorDidMount}
+                    onChange={(value, event) => {
+                        if (socket == null) return;
 
-            socket.emit("sendMessageToRoom", {
-              room: roomId,
-              message: value,
-              from: username,
-              to: username,
-            });
-          }}
-          theme={`vs-${theme}`}
-          sx={{
-            borderBottomLeftRadius: "8px",
-          }}
-        />
-        {/* <RunSubmit editorRef={editorRef} /> */}
-      </div>
-    </div>
-  );
+                        socket.emit("sendMessageToRoom", {
+                            room: roomId,
+                            message: value,
+                            from: username,
+                            to: username,
+                        });
+                    }}
+                    theme={`vs-${theme}`}
+                    sx={{
+                        borderBottomLeftRadius: "8px",
+                    }}
+                />
+                {/* <RunSubmit editorRef={editorRef} /> */}
+            </div>
+        </div>
+    );
 };
 
 export default CodeEditor;
