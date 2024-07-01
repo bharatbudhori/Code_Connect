562118ddda010f2ff7cb2eeffc31af82c89b256f
Anshdeep singh <84078845+Ansh-2002@users.noreply.github.com>
2024-07-01 18:10:46 +0530

    replace backend url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
-export const serverUrl = 'http://localhost:5000/';
+export const serverUrl = 'http://code-dual.vercel.app/';
 
-export const API_URL = 'http://localhost:5000';
+export const API_URL = 'http://code-dual.vercel.app';
 
 export const tempCode = `#include <iostream>
 
